f4a529763b0271507bdabd4bb004ab45b7ec4901
Jake Anderson <ajakef@gmail.com>
2012-07-30 00:00:00 +0000

    version 1.1

--- R/MakeAtmList.lin.R
+++ R/MakeAtmList.lin.R
@@ -1,7 +1,7 @@
 MakeAtmList.lin = function(z0 = 0, c0 = 343, gc = 0, wx0 = 0, gwx = 0, wy0 = 0, gwy = 0, rho0 = 1.2929 * exp(-z0/6800), grho = -0.0001901323 * exp(-z0/6800)){
   # it makes the most sense for z0 to be a scalar, but it won't crash if it's a vector.  Anything else can be a vector or scalar.  Makes a list of ATMs.
   # replace gc = 0 with gc = epsilon
-  gc[gc == 0] = 10^-6
+  gc[gc == 0] = 10^-9
 
   # make every possible combination out of the input variables.  
   out = meshgridn(list(z0, c0, gc, wx0, gwx, wy0, gwy, rho0, grho))
--- src/AtmRay.c
+++ src/AtmRay.c
@@ -8,7 +8,7 @@ void proplin(double *p, double *az, const double *zs, const double *zr, const do
 {
   //  Rprintf("p %f, az %f, zs %f, zr %f, c0 %f, gc %f, wx0 %f, gwx %f, wy0 %f, gwy %f, rho0 %f, grho %f, x %f, y %f, t %f, A %f", *p, *az, *zs, *zr, *c0, *gc, *wx0, *gwx, *wy0, *gwy, *rho0, *grho, *x, *y, *t, *A);
 
-  double u0, v0, c0_eff, gu, gv, gc_eff, cs, cr, epss, epsr, rad, T, dpdx, E, lambdar, tan;
+  double u0, v0, c0_eff, gu, gv, gc_eff, cs, cr, epss, epsr, rad, T, dpdx, E, lambdar, tan, us, ur, dthdx;
   double pi = M_PI;
   double p_eps = pow(10, -8);
   double gc_eps = pow(10, -5);
@@ -33,34 +33,88 @@ void proplin(double *p, double *az, const double *zs, const double *zr, const do
   //Rprintf("2\n");
 
   // Define source/receiver effective sound speed
-  cs = c0_eff + *zs * gc_eff;
-  cr = c0_eff + *zr * gc_eff;
+  //  cs = c0_eff + *zs * gc_eff;
+  //  cr = c0_eff + *zr * gc_eff;
+  cs = *c0 + *zs * *gc;
+  cr = *c0 + *zr * *gc;
+  us = u0 + *zs * gu;
+  ur = u0 + *zr * gu;
   //Rprintf("3\n");
   
   // These are useful variables to define separately
-  epss = sqrt(1 - *p * *p * cs * cs);
-  epsr = sqrt(1 - *p * *p * cr * cr);
+  //  epss = sqrt(1 - *p * *p * cs * cs);
+  //  epsr = sqrt(1 - *p * *p * cr * cr);
   // Calculate displacement, time, amplitude
   // Equations from Jeff's notes for Villarrica paper from Slotnick
-  rad = (*zs - *zr) * (epss - epsr)/(cr - cs)/ *p; // radial distance traveled
+  // JFA 9/13/2012: YOU CANNOT USE EFFECTIVE SOUND SPEED IN 3D!!! Rewriting using equations from Garces and numeric integration (farther down).
+  //  rad = (*zs - *zr) * (epss - epsr)/(cr - cs)/ *p; // radial distance traveled//BAD EQUATION!
+  
   //Rprintf("4\n");
   //Rprintf("%f\n", (*zs - *zr)/(cs - cr));
   //Rprintf("%f\n", (cs * (1 + epsr))/(cr * (1 + epss)));
   //Rprintf("%f\n", log((cs * (1 + epsr))/(cr * (1 + epss))));
   //Rprintf("%f\n", (*zs - *zr)/(cs - cr) * log((cs * (1 + epsr))/(cr * (1 + epss))));
-  *t = fabs((*zs - *zr)/(cs - cr) * log((cs * (1 + epsr))/(cr * (1 + epss)))); // traveltime
- 
+  //*t = fabs((*zs - *zr)/(cs - cr) * log((cs * (1 + epsr))/(cr * (1 + epss)))); // traveltime
+
+  // numerical integration (trapezoidal) of equations from Garces et al.
+  *t = 0;
+  rad = 0;
+  tan = 0;
+  int nsteps = 100;
+  double dz = (*zs - *zr)/nsteps;
+  double zi1, zi2, s1, s2, u1, u2, sqrt1, sqrt2;
+  double p2 = *p + pow(10, -9); // for use in computing rad2, which is used to find dp/dx
+  double rad2 = 0;
+  double wind_offset = 0;
+  double wind_offset2 = 0;
+  for(int i = 0; i < nsteps; i++){
+    zi1 = *zr + i * dz;
+    zi2 = *zr + (i+1)*dz;
+    s1 = 1/(*c0 + *gc * zi1);
+    s2 = 1/(*c0 + *gc * zi2);
+    u1 = u0 + gu * zi1;
+    u2 = u0 + gu * zi2;
+    sqrt1 = sqrt(pow(s1, 2) - *p * *p/pow(1 - *p * u1, 2));
+    sqrt2 = sqrt(pow(s2, 2) - *p * *p/pow(1 - *p * u2, 2));
+    *t += 0.5 * (pow(s1, 2) / sqrt1 + pow(s2, 2) / sqrt2 ) * fabs(dz);
+    rad += 0.5 * ( (*p/(1 - *p * u1) + pow(s1,2) * u1)/sqrt1 + (*p/(1 - *p * u2) + pow(s2,2) * u2)/sqrt2 ) * fabs(dz);
+    tan += 0.5 * ( pow(s1, 2) * (v0 + gv * zi1)/sqrt1 + pow(s2, 2) * (v0 + gv * zi2)/sqrt2) * fabs(dz);
+    wind_offset += 0.5 * (u1 * pow(s1, 2) / sqrt1 + u2 * pow(s2, 2) / sqrt2 ) * fabs(dz); // "drift" from wind, used for energy equation.
+    sqrt1 = sqrt(pow(s1, 2) - p2 * p2/pow(1 - p2 * u1, 2));
+    sqrt2 = sqrt(pow(s2, 2) - p2 * p2/pow(1 - p2 * u2, 2));
+    rad2 += 0.5 * ( (p2/(1 - p2 * u1) + pow(s1,2) * u1)/sqrt1 + (p2/(1 - p2 * u2) + pow(s2,2) * u2)/sqrt2 ) * fabs(dz);
+    wind_offset2 += 0.5 * (u1 * pow(s1, 2) / sqrt1 + u2 * pow(s2, 2) / sqrt2 ) * fabs(dz); // "drift" from wind, used for energy equation.
+  }
+    //  Rprintf("%f, %f, %f, %f, %f, %f, %f, %f, %f, %f, %f\n", rad, tan, *t, zi1, zi2, s1, s2, u1, u2, sqrt1, sqrt2);
+		    
   // Calculate amplitude (several steps)
   //Rprintf("%f, %f, %f, %f, %f, %f, %f\n", cs, cr, *p, epsr, epss, *zs, *zr);
-  dpdx = (cs - cr) * *p * *p * epsr * epss/ ((*zs - *zr) * (epss * epss * epsr - epsr * epsr * epss + epsr * *p * *p * cs * cs - epss * *p * *p * cr * cr)); //d(ray parameter)/d(radial distance): used to calculate energy
+  //dpdx = (cs - cr) * *p * *p * epsr * epss/ ((*zs - *zr) * (epss * epss * epsr - epsr * epsr * epss + epsr * *p * *p * cs * cs - epss * *p * *p * cr * cr)); //d(ray parameter)/d(radial distance): used to calculate energy
   //Rprintf("5\n");
-  E = *p * cs * cs/(rad * (1 - *p * *p * cs * cs)) * dpdx; // arrival energy density
+    
+  //  dpdx = (ps1 - ps2)/(rad - rad2);
+  //E = *p * cs * cs/(rad * (1 - *p * *p * cs * cs)) * dpdx; // arrival energy densiy
+  //  E = ps1 * cs * cs/(rad * (1 - ps1 * ps1 * cs * cs)) * dpdx; // arrival energy density
+
+  //  double ths = atan2(cs * cs/(1/ *p - us) + us, cs * sqrt(1-pow(cs/(1/ *p - us), 2)));
+  //  double thr = atan2(cr * cr/(1/ *p - ur) + ur, cr * sqrt(1-pow(cr/(1/ *p - ur), 2)));
+  //  double ths2 = atan2(cs * cs/(1/ p2 - us) + us, cs * sqrt(1-pow(cs/(1/p2 - us), 2)));
+
+  double ths = asin(cs/(1/ *p - us));
+  double ths2 = asin(cs/(1/p2 - us));
+  double thr = asin(cr/(1/ *p - ur));
+
+  dthdx = (ths - ths2)/(rad - rad2 + wind_offset2 - wind_offset);
+  E = sin(ths) * dthdx/(4 * pi * (rad - wind_offset) * cos(thr));
+  
+  //  Rprintf("%f, %f, %f, %f, %f, %f, %f, %f\n", ths, thr, ths2, dthdx, wind_offset, rad, rad2, E);
+
   //Rprintf("6\n");
   lambdar = pow(*c0 + *gc * *zr, 2) * (*rho0 + *grho * *zr); // the lame' parameter at the receiver
   *A = sqrt(lambdar * E); // arrival amplitude
 
   // calculate transverse motion (ray can travel perpendicular to azimuth with a transverse wind)
-  tan = gv/(gc_eff*gc_eff) * asin(cs * *p)/ *p + (c0_eff * gv - v0 * gc_eff)/(gc_eff * gc_eff) * log((1 / *p + sqrt(1/(*p * *p) - cs*cs))/cs) - (gv/(gc_eff * gc_eff) * asin(cr * *p)/ *p + (c0_eff*gv - v0*gc_eff)/(gc_eff * gc_eff) * log((1/ *p + sqrt(1/(*p * *p) - cr*cr))/cr));  // tangential motion: did this by hand over multiple days--int v dT = int v dT/dz dz
+	  //  tan = gv/(gc_eff*gc_eff) * asin(cs * *p)/ *p + (c0_eff * gv - v0 * gc_eff)/(gc_eff * gc_eff) * log((1 / *p + sqrt(1/(*p * *p) - cs*cs))/cs) - (gv/(gc_eff * gc_eff) * asin(cr * *p)/ *p + (c0_eff*gv - v0*gc_eff)/(gc_eff * gc_eff) * log((1/ *p + sqrt(1/(*p * *p) - cr*cr))/cr));  // tangential motion: did this by hand over multiple days--int v dT = int v dT/dz dz
   //Rprintf("7\n");
  
   // Finally: rotate rad, tan to find outputs x and y
@@ -103,6 +157,17 @@ void p4xlin(double *x, double *y, const double *zs, const double *zr, const doub
   *error = 1.0/0.0;
 
   *az = atan2(*x, *y) * 180/pi;
+
+  // we can use a shortcut if wind and dc/dz are zero: just propagate in a straight line
+  double eps = 0.000001;
+  if(fabs(*gc) < eps && fabs(*wx0) < eps && fabs(*wy0) < eps && fabs(*gwx) < eps && fabs(*gwy) < eps){
+    *p = sqrt(*x * *x + *y * *y)/sqrt(*x * *x + *y * *y + pow(*zs - *zr, 2))/ *c0;
+    proplin(p, az, zs, zr, c0, gc, wx0, gwx, wy0, gwy, rho0, grho, &mx, &my, &t, &Amp); // this call changes mx and my
+
+    *error = sqrt(pow(mx - *x, 2) + pow(my - *y, 2));
+    return;
+  }
+
   // loop until error is acceptable
   while(*error > *maxerror){
     //Rprintf("%f, %f, %f, %f, %f, \n", *p, *az, *error, mx, my);
